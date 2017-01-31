98ebb340417c38c427185564be8f016664e2c991
niksmac <hello@nikhilmohan.in>
2017-01-31 12:41:02 +0530

    Updated Chat interaction

--- scripts/ui.js
+++ scripts/ui.js
@@ -7,8 +7,10 @@ jQuery(document).ready(function ($) {
   var meBeep = new Audio('beeps/me.mp3')
 
   // 1
-  messageRow('What can i do for you?', 'left')
-  showChoice()
+  setTimeout(function () {
+    messageRow('What can i do for you?', 'left')
+    showChoice()
+  }, 1500)
 
   function messageRow (txt, side) {
     focusMessage()
@@ -27,12 +29,10 @@ jQuery(document).ready(function ($) {
 
   function showChoice () {
     setTimeout(function () {
-      $chatDiv.append('<div class="messagebox"> \
-        <div class="ui grid"> \
+      $('#infoSection').append('<div class="ui grid"> \
           <div class="ten wide column"><a data-actn="answer" class="ui right pointing label">answer</a></div>\
-          <div class="six wide column right aligned"><a data-actn="plus" class="ui black label">+</a></div>\
-        </div> \
-      </div>')
+          <div class="five wide column right aligned"><a data-actn="plus" class="ui black label">+</a></div>\
+        </div>')
       initChoiceClick()
       botBeep.play()
     }, 1000)
@@ -77,32 +77,61 @@ jQuery(document).ready(function ($) {
 
   function initActionsClick () {
     $('.ui.actions a').on('click', function () {
-      jQuery('#file').trigger('click')
+      let action = $(this).attr('data-clik')
+      switch (action) {
+        case 'search':
+
+          break
+        case 'camera':
+          $('.ui.modal').modal('show')
+          navigator.webkitGetUserMedia({video: true},
+            function (stream) {
+              document.getElementById('camera').src = URL.createObjectURL(stream)
+            },
+            function () {
+              alert('could not connect stream')
+            }
+          )
+          break
+        case 'attach':
+          $('#file').trigger('click')
+          break
+        default:
+
+      }
     })
   }
 
   function showActionList () {
-    var theList = '<div class="ui basic labels actions"> \
-        <a class="ui horizontal label"> Fun </a> \
-        <a class="ui horizontal label"> Happy </a> \
-        <a class="ui label"> Smart </a> \
-        <a class="ui label"> Insane </a> \
-        <a class="ui label"> Exciting </a> \
+    var theList = '<div class="ui circular labels big actions"> \
+        <a class="ui label" data-clik="camera"> <i class="camera retro icon"></i> </a> \
+        <a class="ui label" data-clik="attach"> <i class="attach icon"></i> </a> \
+        <a class="ui label" data-clik="attach"> <i class="file video outline icon"></i> </a> \
+        <a class="ui label" data-clik="attach"> <i class="marker icon"></i> </a> \
+        <a class="ui label" data-clik="attach"> <i class="lock icon"></i> </a> \
+        <a class="ui label" data-clik="search"> <i class="search icon"></i> </a> \
       </div>'
     $chatDiv.append(theList)
     botBeep.play()
+    initActionsClick()
   }
 
   function prepareInput () {
+    $('body').addClass('chatsthere')
     $('#chatInput').removeClass('hidden slideOutDown')
     $('#chatInput').addClass('animated slideInUp')
     $('#msgTxt').focus()
   }
 
   function hideInput () {
+    $('body').removeClass('chatsthere')
     $('#chatInput').removeClass('animated slideInUp')
     $('#chatInput').addClass('animated slideOutDown')
   }
+
+  $('.ui.modal').on('click', function () {
+    $('.ui.modal').modal('show')
+  })
 })
 
 // setInterval(function () {
